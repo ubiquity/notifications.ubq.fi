29031dd5fd8aed24c592accbf46979eddb8c96c5
zugdev <zug.dev@proton.me>
2024-12-08 13:39:52 -0300

    feat: add GitHub notification icon

--- src/home/rendering/render-github-issues.ts
+++ src/home/rendering/render-github-issues.ts
@@ -86,12 +86,40 @@ function setUpIssueElement(
   const image = `<img />`;
 
   issueElement.innerHTML = `
-      <div class="info"><div class="title"><h3>${
-        notification.notification.subject.title
-      }</h3></div><div class="partner"><p class="organization-name">${organizationName}</p><p class="repository-name">${repositoryName}</p></div></div><div class="labels">${labels.join(
-        ""
-      )}${image}</div>`;
+    <div class="info">
+      <div class="notification-icon"></div>
+      <div class="text-info">
+        <div class="title">
+          <h3>${notification.notification.subject.title}</h3>
+        </div>
+        <div class="partner">
+          <p class="organization-name">${organizationName}</p>
+          <p class="repository-name">${repositoryName}</p>
+        </div>
+      </div>
+    </div>
+    <div class="labels">
+      ${labels.join("")}
+      ${image}
+    </div>`;
 
+  const notificationIcon = issueElement.querySelector(".notification-icon");
+
+  if (notification.notification.subject.type === "Issue" && notificationIcon) {
+      notificationIcon.innerHTML = `
+      <svg class="octicon octicon-issue-opened" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true">
+        <path fill="#3fb950" d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3Z"></path>
+        <path fill="#3fb950" d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0ZM1.5 8a6.5 6.5 0 1 0 13 0 6.5 6.5 0 0 0-13 0Z"></path>
+      </svg>
+      `;
+  } else if (notification.notification.subject.type === "PullRequest" && notificationIcon) {
+      notificationIcon.innerHTML = `
+      <svg class="octicon octicon-git-pull-request  color-fg-open" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true">
+        <path fill="#3fb950" d="M1.5 3.25a2.25 2.25 0 1 1 3 2.122v5.256a2.251 2.251 0 1 1-1.5 0V5.372A2.25 2.25 0 0 1 1.5 3.25Zm5.677-.177L9.573.677A.25.25 0 0 1 10 .854V2.5h1A2.5 2.5 0 0 1 13.5 5v5.628a2.251 2.251 0 1 1-1.5 0V5a1 1 0 0 0-1-1h-1v1.646a.25.25 0 0 1-.427.177L7.177 3.427a.25.25 0 0 1 0-.354ZM3.75 2.5a.75.75 0 1 0 0 1.5.75.75 0 0 0 0-1.5Zm0 9.5a.75.75 0 1 0 0 1.5.75.75 0 0 0 0-1.5Zm8.25.75a.75.75 0 1 0 1.5 0 .75.75 0 0 0-1.5 0Z"></path>
+        </svg>
+      `;
+  }
+  
   issueElement.addEventListener("click", () => {
     try {
       const issueWrapper = issueElement.parentElement;
