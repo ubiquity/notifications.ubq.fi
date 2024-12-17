7d02b1e410aec4da4e2dd316dc00cbfb9e9b1c9f
zugdev <zug.dev@proton.me>
2024-12-17 02:53:52 -0300

    feat: default sort by backlinks in second

--- src/home/sorting/sort-controller.ts
+++ src/home/sorting/sort-controller.ts
@@ -2,6 +2,7 @@ import { GitHubAggregated } from "../github-types";
 import { Sorting } from "./generate-sorting-buttons";
 import { sortBy } from "./sort-by";
 import { sortByPriority } from "./sort-by-priority";
+import { sortByBacklinks } from "./sort-by-backlinks";
 import { sortByActivity } from "./sort-by-activity";
 
 export function sortIssuesController(tasks: GitHubAggregated[], sorting?: Sorting, options = { ordering: "normal" }) {
@@ -10,8 +11,12 @@ export function sortIssuesController(tasks: GitHubAggregated[], sorting?: Sortin
   if (sorting) {
     sortedNotifications = sortBy(sortedNotifications, sorting);
   } else {
-    const sortedByFreshness = sortByActivity(sortedNotifications); // activity first
-    const sortedByPriority = sortByPriority(sortedByFreshness); // highest priority first
+    const sortedByFreshness = sortByActivity(sortedNotifications);  // activity last
+    console.log("sortedByFreshness", sortedByFreshness);
+    const sortedByBacklinks = sortByBacklinks(sortedByFreshness);   // backlinks second
+    console.log("sortedByBacklinks", sortedByBacklinks);
+    const sortedByPriority = sortByPriority(sortedByBacklinks);     // highest priority first
+    console.log("sortedByPriority", sortedByPriority);
     sortedNotifications = sortedByPriority;
   }
 
