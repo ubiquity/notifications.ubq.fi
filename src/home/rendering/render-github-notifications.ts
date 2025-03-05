9ac6f8bdd425805a7d404d65d876d382c3a53d87
0x4007 <gpg@pavlovcik.com>
2025-03-06 02:49:14 +0900

    refactor: improve GitHub notifications empty state rendering

--- src/home/rendering/render-github-notifications.ts
+++ src/home/rendering/render-github-notifications.ts
@@ -65,15 +65,19 @@ export async function renderEmpty() {
   issueElement.innerHTML = `
     <div class="info">
       <div class="notification-icon">
-        ${isLoggedIn ? `
+        ${
+          isLoggedIn
+            ? `
           <svg aria-label="All notifications read" role="img" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-check">
               <path fill="#1a7f37" d="M13.78 4.22a.75.75 0 0 1 0 1.06l-7.25 7.25a.75.75 0 0 1-1.06 0L2.22 9.28a.751.751 0 0 1 .018-1.042.751.751 0 0 1 1.042-.018L6 10.94l6.72-6.72a.75.75 0 0 1 1.06 0Z"></path>
           </svg>
-        ` : `
+        `
+            : `
           <svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-sign-in">
               <path d="M2 2.75C2 1.784 2.784 1 3.75 1h2.5a.75.75 0 0 1 0 1.5h-2.5a.25.25 0 0 0-.25.25v10.5c0 .138.112.25.25.25h2.5a.75.75 0 0 1 0 1.5h-2.5A1.75 1.75 0 0 1 2 13.25Zm6.56 4.5h5.69a.75.75 0 0 1 0 1.5H8.56l1.97 1.97a.75.75 0 0 1-1.06 1.06L6.22 8.53a.75.75 0 0 1 0-1.06l3.25-3.25a.75.75 0 1 1 1.06 1.06L8.56 7.25Z"></path>
           </svg>
-        `}
+        `
+        }
       </div>
       <div class="text-info">
         <div class="title">
